4db0579d9cae5d41a79159538a4d063d48878508
Jiang <cyanogensky@gmail.com>
2017-12-05 09:25:05 +0800

    大图片压缩

--- SDWebImage/SDWebImageDownloaderOperation.m
+++ SDWebImage/SDWebImageDownloaderOperation.m
@@ -385,6 +385,9 @@ NSString *const SDWebImageDownloadFinishNotification = @"SDWebImageDownloadFinis
             NSString *key = [[SDWebImageManager sharedManager] cacheKeyForURL:self.request.URL];
             image = [self scaledImageForKey:key image:image];
             
+            NSData *data = UIImageJPEGRepresentation(image, 1);
+            self.imageData = [NSMutableData dataWithData:data];
+            
             // Do not force decoding animated GIFs
             if (!image.images) {
                 if (self.shouldDecompressImages) {
